e42c86f2764f20fba20bcdf9d9d5ba41931ed7a2
aayush1332 <bhandariaayush21@gmail.com>
2024-06-28 16:53:03 +0545

    database issue

---
+++ frontend/src/components/Home/PopularCategories.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  MdOutlineDesignServices,
+  MdOutlineWebhook,
+  MdAccountBalance,
+  MdOutlineAnimation,
+} from "react-icons/md";
+import { TbAppsFilled } from "react-icons/tb";
+import { FaReact } from "react-icons/fa";
+import { GiArtificialIntelligence } from "react-icons/gi";
+import { IoGameController } from "react-icons/io5";
+
+const PopularCategories = () => {
+  const categories = [
+    
+      
+  ];
+  return (
+    <div className="categories">
+      <h3></h3>
+      <div className="banner">
+        {categories.map((element) => {
+          return (
+            <div className="card" key={element.id}>
+              <div className="icon">{element.icon}</div>
+              <div className="text">
+                <p>{element.title}</p>
+                <p>{element.subTitle}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default PopularCategories;
